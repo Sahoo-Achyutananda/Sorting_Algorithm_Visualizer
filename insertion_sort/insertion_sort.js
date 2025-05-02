76b24fcbe4230b71c1abf98fcfa73b3e4511aa6a
ACHYUTANANDA-SAHOO <achu23022002@gmail.com>
2025-05-02 11:29:56 +0530

    Initiated Insertion Sort

---
+++ insertion_sort/insertion_sort.js
@@ -0,0 +1,104 @@
+import * as util from "../utils/utils.js" ;
+
+function checkSpeed(){
+    let speed = document.querySelector("#speed_slider").value;
+    let speedFactor = 1/speed ;
+    return speedFactor;
+}
+
+export async function insertionSort(signal){
+    let boxes = document.querySelectorAll(".box");
+    let heights = [];
+    let speedFactor = checkSpeed();
+
+    boxes.forEach((box)=>{
+        heights.push(parseInt(box.style.height));
+    })
+
+    let swaps = 0;
+    let comparisons = 0;
+
+    let currentNum = document.getElementById("current_num");
+    let neighbourNum = document.getElementById("adjacent_num");
+    let minNum = document.getElementById("minimum_num");
+    let swapNum = document.getElementById("swap_num");
+    let comparisonNum = document.getElementById("comparison_num");
+
+    let startBtn = document.querySelector(".start");
+    
+    for(let i = 1; i< heights.length; i++){
+
+        if(signal.aborted){
+            return;
+        }
+
+        let key = boxes[i].style.height;
+        let j = i - 1;
+        speedFactor = checkSpeed();
+        
+        boxes[i].classList.add('current');
+        currentNum.textContent = parseInt(boxes[i].style.height);
+        
+
+        while(j >=0 && parseInt(boxes[j].style.height > key)){
+            speedFactor = checkSpeed();
+            // detect RESET and abort the current execution
+            if(signal.aborted){
+                return;
+            }
+
+            boxes.forEach(box => {
+                box.style.transition = `height ${500*speedFactor}ms ease`;
+            });
+
+            boxes[j].classList.add('compared');
+            await util.randomDelay(500*speedFactor);
+            neighbourNum.textContent = parseInt(boxes[j].style.height);
+            
+            comparisons++;
+            comparisonNum.textContent = comparisons;
+
+            if(parseInt(window.getComputedStyle(boxes[j]).height) < parseInt(window.getComputedStyle(boxes[min_index]).height)){
+                let prevMins = document.querySelectorAll(".min_element");
+                if(prevMins){
+                    prevMins.forEach((x)=>{
+                        x.classList.remove('min_element');
+                    });
+                }
+                min_index = j;
+                minNum.textContent = parseInt(window.getComputedStyle(boxes[min_index]).height);
+                boxes[min_index].classList.add('min_element');
+            }
+            boxes[j].classList.remove('compared');
+        }
+    
+        boxes[i].classList.add('swap');
+        boxes[min_index].classList.add('swap');
+
+        // the following line has to be here - not before add "swap" 
+        boxes[min_index].classList.remove('min_element');
+        minNum.textContent = "";
+        
+        if(i != min_index){
+            util.swapStyles(boxes[i],boxes[min_index]);
+            swaps++;
+            swapNum.textContent = swaps;
+        }
+
+        document.getElementById("swap_num").classList.add('swapped'); // expecting a blinking effect whenever a swap happens
+        await util.randomDelay(500*speedFactor);
+
+
+        boxes[i].classList.remove('swap');
+        boxes[min_index].classList.remove('swap');
+        document.getElementById("swap_num").classList.remove('swapped');
+
+        boxes[i].classList.remove('current');
+
+        if(boxes[min_index])
+            boxes[min_index].classList.remove('compared');
+
+        await util.randomDelay(500*speedFactor);
+    }
+    
+}
---
+++ insertion_sort/main.js
@@ -0,0 +1,112 @@
+import { fillContainer, clearContainer, getRandomHeight, sayHello } from "../utils/utils.js";
+import {insertionSortSort} from "./insertion_sort.js";
+
+let mainBox = document.querySelector(".main_box");
+let countSlider = document.querySelector("#count_slider");
+let countText = document.querySelector("#count_value");
+
+let speedSlider = document.querySelector("#speed_slider");
+let speedValue = document.querySelector('#speed_value');
+
+let startBtn = document.querySelector(".start");
+let resetBtn =document.querySelector(".reset");
+
+
+let currentNum = document.getElementById("current_num");
+let neighbourNum = document.getElementById("adjacent_num");
+let swapNum = document.getElementById("swap_num");
+let comparisonNum = document.getElementById("comparison_num");
+let minNum = document.getElementById("minimum_num");
+
+let controller = null;
+
+window.onload = function(){
+
+    fillContainer(); // affects only when the page is loaded for the first time , for reloads it doesnt work
+}
+
+resetBtn.addEventListener("click",()=>{
+
+    if(controller) {
+        controller.abort(); // stop the current sort
+    }
+
+    startBtn.disabled = false;
+    countText.value = countSlider.value;
+    clearContainer();
+    fillContainer(countText.value);
+
+})
+
+// updating slider value based on input value and viceversa
+countSlider.addEventListener("input", ()=>{
+    countText.value = countSlider.value;
+    if(controller) {
+        controller.abort(); // stop the current sort
+    }
+    clearContainer();
+    fillContainer(countText.value);
+
+})
+
+countText.addEventListener("input", ()=>{
+    countText.value = countSlider.value;
+    clearContainer();
+    fillContainer(countSlider.value);
+})
+
+// updating speed value based on input value and viceversa
+speedSlider.addEventListener("input", ()=>{
+    speedValue.value = speedSlider.value;
+
+})
+
+speedValue.addEventListener("input", ()=>{
+    speedSlider.value = speedValue.value;
+})
+
+startBtn.addEventListener("click", async ()=>{
+    // clearContainer();
+    // fillContainer(countText.value);
+    
+    
+    startBtn.disabled = "true";
+
+    controller = new AbortController();
+    await selectionSort(controller.signal);
+
+    document.getElementById("current_num").textContent = "";
+    document.getElementById("adjacent_num").textContent = "";
+    swapNum.textContent = "";
+    comparisonNum.textContent = "";
+    minNum.textContent = "";
+    // swapNum.textContent = "";
+    // comparisonNum.textContent = "";
+
+    // console.log("Done");
+})
+
+
+
+// reloading + function invokation : but this solution works only when there is a button in the page
+// that reloads the page ||| it wont work when the browser's reload button is pressed !
+
+// function reloadPage(){
+//     sessionStorage.setItem("reload", "true");
+// }
+
+// window.onload = function(){
+//     const reloading = sessionStorage.getItem("reloading");
+//     if(reloading){
+//         sessionStorage.removeItem("reloading");
+//         fillContainer();
+//     }
+// }
+
+window.addEventListener('load', () => {
+    if (performance.getEntriesByType('navigation') === 'reload') {
+        // Reload detected
+        fillContainer(); 
+    
+    }
+});
