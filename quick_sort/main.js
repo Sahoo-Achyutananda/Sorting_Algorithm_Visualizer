781e121bba0b59f5545fdfd8cda5989f998b6eed
ACHYUTANANDA-SAHOO <achu23022002@gmail.com>
2025-05-03 15:44:09 +0530

    Quick ort Initiated

---
+++ quick_sort/main.js
@@ -0,0 +1,124 @@
+// import { fillContainer, clearContainer, initializeValues, getRandomHeight, sayHello } from "../utils/utils.js";
+// import {mergeSort} from "./merge_sort.js";
+
+// let mainBox = document.querySelector(".main_box");
+// let countSlider = document.querySelector("#count_slider");
+// let countText = document.querySelector("#count_value");
+
+// let speedSlider = document.querySelector("#speed_slider");
+// let speedValue = document.querySelector('#speed_value');
+
+// let startBtn = document.querySelector(".start");
+// let resetBtn =document.querySelector(".reset");
+
+
+// let currentNum = document.getElementById("current_num");
+// let neighbourNum = document.getElementById("adjacent_num");
+// let swapNum = document.getElementById("swap_num");
+// let comparisonNum = document.getElementById("comparison_num");
+
+
+// let controller = null;
+
+// window.onload = function(){
+
+//     fillContainer(); // affects only when the page is loaded for the first time , for reloads it doesnt work
+// }
+
+// function clearUp(){
+
+//     if(controller) {
+//         controller.abort(); // stop the current sort
+//     }
+
+//     currentNum.textContent = "";
+//     neighbourNum.textContent = "";
+//     swapNum.textContent = "";
+//     comparisonNum.textContent = "";
+
+//     startBtn.disabled = false;
+//     countText.value = countSlider.value;
+//     clearContainer();
+//     fillContainer(countText.value);
+
+// }
+
+// resetBtn.addEventListener("click", ()=>{
+//     clearUp();
+// })
+
+// // updating slider value based on input value and viceversa
+// countSlider.addEventListener("input", ()=>{
+//     countText.value = countSlider.value;
+//     clearUp();
+// })
+
+// countText.addEventListener("input", ()=>{
+//     countText.value = countSlider.value;
+//     clearUp();
+// })
+
+// // updating speed value based on input value and viceversa
+// speedSlider.addEventListener("input", ()=>{
+//     speedValue.value = speedSlider.value;
+
+// })
+
+// speedValue.addEventListener("input", ()=>{
+//     speedSlider.value = speedValue.value;
+// })
+
+
+
+// // startBtn.addEventListener("click", async ()=>{
+// //     // clearContainer();
+// //     // fillContainer(countText.value);
+
+// //     // let boxes = document.querySelectorAll(".box");
+// //     startBtn.disabled = "true";
+// //     swapNum.textContent = "";
+// //     comparisonNum.textContent = "";
+
+// //     let boxes = initializeValues();
+// //     console.log(boxes);
+// //     controller = new AbortController();
+// //     await mergeSort(controller.signal,boxes,0,boxes.length-1);
+
+// //     // let h = []
+// //     // boxes.forEach((x)=>{
+// //     //     h.push(x.style.height);
+// //     // })
+
+// //     console.log(h);
+
+// //     currentNum.textContent = "";
+// //     neighbourNum.textContent = "";
+// //     // comparisonNum.textContent = "";
+
+// //     startBtn.disabled = false;
+// // })
+
+
+
+// // reloading + function invokation : but this solution works only when there is a button in the page
+// // that reloads the page ||| it wont work when the browser's reload button is pressed !
+
+// // function reloadPage(){
+// //     sessionStorage.setItem("reload", "true");
+// // }
+
+// // window.onload = function(){
+// //     const reloading = sessionStorage.getItem("reloading");
+// //     if(reloading){
+// //         sessionStorage.removeItem("reloading");
+// //         fillContainer();
+// //     }
+// // }
+
+// window.addEventListener('load', () => {
+//     if (performance.getEntriesByType('navigation') === 'reload') {
+//         // Reload detected
+//         fillContainer(); 
+    
+//     }
+// });
---
+++ quick_sort/quick_sort.js
@@ -0,0 +1,162 @@
+import * as util from "../utils/utils.js";
+import { fillContainer, clearContainer, initializeValues, getRandomHeight, sayHello } from "../utils/utils.js";
+
+
+let values = [];
+let speedFactor = 500;
+let swaps = 0;
+let comparisons = 0;
+let startBtn = document.querySelector(".start");
+
+// import {mergeSort} from "./merge_sort.js";
+
+let mainBox = document.querySelector(".main_box");
+let countSlider = document.querySelector("#count_slider");
+let countText = document.querySelector("#count_value");
+
+let speedSlider = document.querySelector("#speed_slider");
+let speedValue = document.querySelector('#speed_value');
+
+// let startBtn = document.querySelector(".start");
+let resetBtn =document.querySelector(".reset");
+
+
+let currentNum = document.getElementById("current_num");
+let neighbourNum = document.getElementById("adjacent_num");
+let swapNum = document.getElementById("swap_num");
+let comparisonNum = document.getElementById("comparison_num");
+
+
+
+let controller = null;
+
+window.onload = function(){
+
+    util.fillContainerWithIndex(); // affects only when the page is loaded for the first time , for reloads it doesnt work
+}
+
+function clearUp(){
+
+    if(controller) {
+        controller.abort(); // stop the current sort
+    }
+
+    // currentNum.textContent = "";
+    neighbourNum.textContent = "";
+    swaps = 0;
+    comparisons = 0;
+    swapNum.textContent = "";
+    comparisonNum.textContent = "";
+    neighbourNum.textContent = "";
+
+    startBtn.disabled = false;
+    countText.value = countSlider.value;
+    util.clearContainerWithIndex();
+    util.fillContainerWithIndex(countText.value);
+
+}
+
+resetBtn.addEventListener("click", ()=>{
+    clearUp();
+})
+
+// updating slider value based on input value and viceversa
+countSlider.addEventListener("input", ()=>{
+    countText.value = countSlider.value;
+    clearUp();
+})
+
+countText.addEventListener("input", ()=>{
+    countText.value = countSlider.value;
+    clearUp();
+})
+
+// updating speed value based on input value and viceversa
+speedSlider.addEventListener("input", ()=>{
+    speedValue.value = speedSlider.value;
+
+})
+
+speedValue.addEventListener("input", ()=>{
+    speedSlider.value = speedValue.value;
+})
+
+window.addEventListener('load', () => {
+    if (performance.getEntriesByType('navigation') === 'reload') {
+        // Reload detected
+        util.fillContainerWithIndex(); 
+    
+    }
+});
+
+async function partition(signal,l, r) {
+    if(signal.aborted){
+        swapNum.textContent = "";
+        comparisonNum.textContent = "";
+        minNum.textContent = "";
+        return;
+    }
+    let speedFactor = util.checkSpeed();
+    util.updateTransitions(speedFactor);
+
+    const boxes = document.querySelectorAll(".box");
+    
+    let pivot  = values[r];
+    let i = l - 1;
+
+    for(let j = l; j<=r-1;j++){
+        if(values[j]<pivot){
+            i++;
+            util.swapStyles(boxes[i],boxes[j]);
+            swap(values,i,j);
+        }
+    }
+    util.swapStyles(boxes[i+1],boxes[r]);
+    swap(values,i+1,r);
+    return i+1;
+
+}
+
+function swap(arr, i, j)
+{
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+export async function quickSort(signal, left, right) {
+    if(signal.aborted){
+        neighbourNum.textContent = "";
+        swapNum.textContent = "";
+        comparisonNum.textContent = "";
+        minNum.textContent = "";
+        return;
+    }
+  if (left >= right) return;
+  
+  const pi = await partition(signal,left,right);
+  await quickSort(signal, left, pi-1);
+  await quickSort(signal, pi + 1, right);
+}
+
+startBtn.addEventListener("click", async () => {
+    startBtn.disabled = true;
+    swapNum.textContent = "0";
+    comparisonNum.textContent = "0";
+    neighbourNum.textContent = "";
+    swaps = 0;
+    comparisons = 0;
+    
+    values = util.initializeValues();
+    
+    controller = new AbortController();
+    
+    try {
+      await quickSort(controller.signal, 0, values.length - 1);
+    } catch (error) {
+      console.log("Sorting stopped:", error);
+    } finally {
+        neighbourNum.textContent = "";
+      startBtn.disabled = false;
+    }
+  });
