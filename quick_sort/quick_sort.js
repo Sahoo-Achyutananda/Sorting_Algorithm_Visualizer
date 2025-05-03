d1042fff5cd8a1698850b71116f4197082c0fe5f
ACHYUTANANDA-SAHOO <achu23022002@gmail.com>
2025-05-03 17:30:54 +0530

    Quick Sort - Incomplete

--- quick_sort/quick_sort.js
+++ quick_sort/quick_sort.js
@@ -99,20 +99,47 @@ async function partition(signal,l, r) {
     let speedFactor = util.checkSpeed();
     util.updateTransitions(speedFactor);
 
-    const boxes = document.querySelectorAll(".box");
-    
+    var boxes = document.querySelectorAll(".box");
+    // console.log(boxes);
     let pivot  = values[r];
+    boxes[r].classList.add('current');
     let i = l - 1;
 
     for(let j = l; j<=r-1;j++){
+        
+        comparisons++;
+        comparisonNum.textContent = `${comparisons}`;
+
+        await util.randomDelay(500*speedFactor);
+        // boxes[j].classList.add('compared');
+        // boxes[r].classList.add('compared');
         if(values[j]<pivot){
             i++;
+            await util.randomDelay(500*speedFactor);
+
+            // boxes[i].classList.add('swap');
+            // boxes[j].classList.add('swap');
             util.swapStyles(boxes[i],boxes[j]);
             swap(values,i,j);
+
+            await util.randomDelay(500*speedFactor);
+            swaps++;
+            swapNum.textContent = `${swaps}`;
+            // boxes[i].classList.remove('swap');
+            // boxes[j].classList.remove('swap');
         }
+        // boxes[j].classList.remove('compared');
+        // boxes[r].classList.remove('compared');
     }
+
+    boxes[r].classList.remove("current");
+
+    await util.randomDelay(500*speedFactor);
     util.swapStyles(boxes[i+1],boxes[r]);
     swap(values,i+1,r);
+    swaps++;
+
+    swapNum.textContent = `${swaps}`;
     return i+1;
 
 }
@@ -125,6 +152,10 @@ function swap(arr, i, j)
 }
 
 export async function quickSort(signal, left, right) {
+    var boxes = document.querySelectorAll(".box");
+    
+
+    neighbourNum.textContent = `index[${left}, ${right}]`
     if(signal.aborted){
         neighbourNum.textContent = "";
         swapNum.textContent = "";
@@ -134,7 +165,19 @@ export async function quickSort(signal, left, right) {
     }
   if (left >= right) return;
   
+  for(let i = left; i<right;i++){
+    boxes[i].classList.add('compared');
+  }
+  boxes[right].classList.add('current');
   const pi = await partition(signal,left,right);
+  
+  currentNum.textContent = `${pi}`;
+
+  for(let i = left; i<right;i++){
+    boxes[i].classList.remove('compared');
+  }
+  boxes[right].classList.remove('current');
+//   boxes[pi].classList.remove('current');
   await quickSort(signal, left, pi-1);
   await quickSort(signal, pi + 1, right);
 }
